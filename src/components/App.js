c5beea0e5e1adeb6648b70deb1c4af75e2d1a2d8
Gulam Mohiuddeen <90964388+gulam159@users.noreply.github.com>
2022-04-23 02:42:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import '../styles/App.css';
 const App = () => {
 const [state, setState] = useState({ value: "square" });
