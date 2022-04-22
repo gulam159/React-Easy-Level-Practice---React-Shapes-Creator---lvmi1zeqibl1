5fed2d471c0cfd90b2f110606003b80064230dbd
Gulam Mohiuddeen <90964388+gulam159@users.noreply.github.com>
2022-04-23 02:38:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import { useState } from "react";
 import '../styles/App.css';
 const App = () => {
+const [state, setState] = useState({ value: "square" });
+  const [data, setData] = useState([]);
 
+  const handleChange = (event) => {
+    setState({ value: event.target.value });
+  };
+  const handleSubmit = () => {
+    setData([...data, { ...state }]);
+  };
   return (
     <div id="main">
       <div id="shape-creator">
-        
+        <select onChange={handleChange}>
+          <option value="square">Square</option>
+          <option value="circle">Circle</option>
+        </select>
+        <button onClick={handleSubmit}>Add Shape</button>
 
       </div>
       <div id="shapes-holder">
+        {data.map((element, idx) => {
+          return (
+            <>
+              <p className={element.value}>{element.value , idx } </p>
+            </>
+          );
+        })}
       </div>
     </div>
   )
